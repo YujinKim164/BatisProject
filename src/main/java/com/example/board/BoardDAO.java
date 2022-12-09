15376134ce9daf5b105833acd25933f533a46202
Your Name <you@example.com>
2022-12-09 12:21:07 +0900

    first commit

--- src/main/java/com/example/board/BoardDAO.java
+++ src/main/java/com/example/board/BoardDAO.java
@@ -1,5 +1,6 @@
 package com.example.board;
 
+import org.apache.ibatis.session.SqlSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.JdbcTemplate;
@@ -13,36 +14,29 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
-
-
 @Repository
 public class BoardDAO {
     @Autowired
-    JdbcTemplate jdbcTemplate;
+    SqlSession sqlSession;
     public int insertBoard(BoardVO vo) {
-        String sql = "insert into BOARD (title, writer, content, category) values("
-                + "'" + vo.getTitle() + "'" + ","
-                + "'" + vo.getWriter() + "'" +  ","
-                + "'" + vo.getContent() + "'" + ","
-                + "'" + vo.getCategory() +"'"+ ")";
-        return jdbcTemplate.update(sql);
+        int result = sqlSession.insert("Board.insertBoard", vo);
+        return result;
     }
     public int deleteBoard(int seq) {
-        String sql = "delete from BOARD where seq = ?";
-        return jdbcTemplate.update(sql, new Object[]{seq});
+        int count = sqlSession.delete("Board.deleteBoard", seq);
+        return count;
     }
     public int updateBoard(BoardVO vo) {
-        String sql = "update BOARD set title=?, writer=?, content=?, category=? where seq=?";
-        return jdbcTemplate.update(sql, new Object[]{vo.getTitle(), vo.getWriter(), vo.getContent(), vo.getCategory(), vo.getSeq()});
+        int up = sqlSession.update("Board.updateBoard", vo);
+        return up;
     }
-
     public BoardVO getBoard(int seq) {
-        String sql = "select * from BOARD where seq=" + seq;
-        return jdbcTemplate.queryForObject(sql, new BoardRowMapper());
+        BoardVO one = sqlSession.selectOne("Board.getBoard", seq);
+        return one;
     }
     public List<BoardVO> getBoardList(){
-        String sql = "select * from BOARD order by regdate desc";
-        return jdbcTemplate.query(sql, new BoardRowMapper());
+        List<BoardVO> list = sqlSession.selectList("Board.getBoardList");
+        return list;
     }
 }
 
